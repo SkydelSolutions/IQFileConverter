c2e1f1cad84d82ef33e66f8ea3e190f8a810f9bd
Mathieu Lizee <mathieu.lizee@skydelsolutions.com>
2018-11-13 13:58:49 -0500

    Add program parameters verificatiomn

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
+#include <iostream>
+
 #include "iq_file_converter.h"
 
-int main(int /* argc */, char* argv[])
+int main(int argc, char* argv[])
 {
+  if (argc < 3)
+  {
+    std::cout << "Wrong number of arguments: Expected 2 and received " << (argc - 1) << std::endl;
+    std::cout << "  - Input metadata file path" << std::endl;
+    std::cout << "  - Output metadata file path" << std::endl;
+    return -1;
+  }
+
   IQFileConverter fileConverter;
   fileConverter.convertIQFile(argv[1], argv[2]);
 
