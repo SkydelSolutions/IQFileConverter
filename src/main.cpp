a200d2eb14375911888b9987ef07bbd53baf1b32
Mathieu Lizee <mathieu.lizee@skydelsolutions.com>
2018-11-13 14:00:04 -0500

    Fix program parameters validation

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[])
 {
-  if (argc < 3)
+  if (argc != 3)
   {
     std::cout << "Wrong number of arguments: Expected 2 and received " << (argc - 1) << std::endl;
     std::cout << "  - Input metadata file path" << std::endl;
